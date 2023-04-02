904bcb31a4ff8f137fdf93776716700878080eb3
Cyderxxv <phamnguyengiakhiem@gmail.com>
2023-04-02 07:53:02 +0700

    star2lte: Initial Evolution 7.x bringup

--- evolution_star2lte.mk
+++ evolution_star2lte.mk
@@ -28,11 +28,17 @@ $(call inherit-product, device/samsung/star2lte/device.mk)
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := arrow_star2lte
+PRODUCT_NAME := evolution_star2lte
 PRODUCT_DEVICE := star2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G965F

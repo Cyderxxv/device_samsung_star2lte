502e529eb6ff3a4cf2ad0c246526116eb7524e2d
Cyder <phamnguyengiakhiem@gmail.com>
2023-06-10 14:11:38 +0700

    star2lte: Derp-ify

--- derp_star2lte.mk
+++ derp_star2lte.mk
@@ -28,11 +28,13 @@ $(call inherit-product, device/samsung/star2lte/device.mk)
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Official-ify
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+DERP_BUILDTYPE := Official
+USE_LEGACY_BOOTANIMATION := true
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := arrow_star2lte
+PRODUCT_NAME := derp_star2lte
 PRODUCT_DEVICE := star2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G965F
